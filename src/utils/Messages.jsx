d462efad2d5955bf753d159ad025d8ccb81f91f2
chasbeen <charles.a.v.thomas@gmail.com>
2023-08-30 16:38:29 +0100

    Hello messsages!

--- src/utils/Messages.jsx
+++ src/utils/Messages.jsx
@@ -1,26 +1,34 @@
 import React, { useRef, useState, useEffect } from "react"; // <-- ADDED useState and useEffect
 
 export const getErrorForClickObjectNodeOnEditor = (name) =>
-  `Error: You clicked on ${name} but this has no data`;
-  const deriveType = (msg) => {
-    const type = msg.split(':')[0].trim();
-  
-    switch (type) {
-      case "Error":
-      case "Warning":
-      case "Info":
-        return type;
-      default:
-        return "Information";
-    }
-  };
+  `Info: You clicked on ${name} but this has no data`;
+const deriveType = (msg) => {
+  const type = msg.split(":")[0].trim();
+
+  switch (type) {
+    case "Error":
+    case "Warning":
+    case "Success":
+      return type;
+    default:
+      return "Info";
+  }
+};
+
 const Messages = ({ message, isVisible, onOutsideClick, duration = 3 }) => {
   // <-- ADDED duration prop with default value
   const svgRef = useRef(null);
-
+  const titleRef = useRef(null); // <-- REF for the title
+  const [titleBBox, setTitleBBox] = useState(null);
   // NEW STATE for countdown
   const [countdown, setCountdown] = useState(duration);
-
+  const mTypeColour = {
+    Error: "red",
+    Warning: "orange",
+    Success: "blue",
+    Info: "green",
+  };
+  const messageColor = mTypeColour[deriveType(message)];
   useEffect(() => {
     if (countdown <= 0) {
       onOutsideClick(); // makes the message disappear
@@ -34,6 +42,11 @@ const Messages = ({ message, isVisible, onOutsideClick, duration = 3 }) => {
   useEffect(() => {
     setCountdown(duration);
   }, [isVisible, duration]);
+  useEffect(() => {
+    if (titleRef.current) {
+      setTitleBBox(titleRef.current.getBBox());
+    }
+  }, [message]);
   const handleOutsideClick = (event) => {
     if (svgRef.current && !svgRef.current.contains(event.target)) {
       onOutsideClick();
@@ -48,9 +61,6 @@ const Messages = ({ message, isVisible, onOutsideClick, duration = 3 }) => {
   const svgWidth = containerWidth * 0.6;
   const svgHeight =
     containerWidth > containerHeight ? svgWidth * 0.618 : svgWidth * 1.618;
-
-  const topPosition = containerHeight / 2 - svgHeight / 2 + "px";
-  const leftPosition = containerWidth / 2 - svgWidth / 2 + "px";
 
   return (
     <div
@@ -71,25 +81,61 @@ const Messages = ({ message, isVisible, onOutsideClick, duration = 3 }) => {
         <rect
           width="100%"
           height="100%"
-          fill="white"
-          stroke="black"
+          stroke={messageColor}
           strokeWidth="5"
         />
+
+{titleBBox && (
+  <rect
+    x={titleBBox.x - (titleBBox.width * 0.15) / 2}  // A bit of padding
+    y={titleBBox.y - titleBBox.height * 0.5}  // Move it up by half the height
+    width={titleBBox.width + titleBBox.width * 0.15} // Including the padding
+    height={titleBBox.height * 1.618} // Making it 1.618 times larger
+    fill="white"
+    rx={titleBBox.width * 0.15}  // 15% of the rectangle's width for rounded corners
+    ry={titleBBox.width * 0.15}  // Same for y-radius
+  />
+)}
+
+<text
+  ref={titleRef}
+  x="50%"
+  y="5%" 
+  dominantBaseline="hanging"
+  textAnchor="middle"
+  fontSize="250%"
+  fill={messageColor}
+>
+  {deriveType(message)}
+</text>
+
+{/* ...rest of the SVG content... */}
+This should adjust the rectangle as specified. The text will now appear centered within this expanded rectangle due to the adjustments in the rectangle's y and height properties. The rectangle will also have the rounded corners as you mentioned.
+
+
+
+
+
+
+
         <text
           x="50%"
           y="50%"
           dominantBaseline="middle"
           textAnchor="middle"
           fontSize="150%"
+          fill={messageColor}
         >
           {message}
         </text>
+
         <text
           x="90%"
           y="90%"
           dominantBaseline="middle"
           textAnchor="middle"
           fontSize="125%" // Adjust size if needed
+          fill={messageColor}
         >
           {countdown} {/* <-- Countdown Timer Display */}
         </text>
